9b9a7641a145881682f0b2c3aa97652fc097cee4
Yuchia <jerryis0812@gmail.com>
2026-01-07 21:19:41 +0800

    0107 sync

--- test_api.py
+++ test_api.py
@@ -8,40 +8,42 @@ url = f"https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001?Authorizat
 
 response = requests.get(url, verify=False)
 
-if response.status_code == 200:
-    data = response.json()
-    print('Get data successfully')
+def get_weather_data(city_name):
 
-    locations = data["records"]["location"]
-    
-    target_city = "臺中市"
-    found_index = -1
+    if response.status_code == 200:
+        data = response.json()
+        print('Get data successfully')
 
-    for i in range(len(locations)):
-        if locations[i]['locationName'] == target_city:
-            found_index = i
-            break
-    if found_index != -1 :
-        print(f'找到{target_city}了, found_index = {found_index}')
+        locations = data["records"]["location"]
+        
+        city_name = "臺中市"
+        found_index = -1
 
-        target_weather_elements = locations[i]['weatherElement']
+        for i in range(len(locations)):
+            if locations[i]['locationName'] == city_name:
+                found_index = i
+                break
+        if found_index != -1 :
+            print(f'找到{city_name}了, found_index = {found_index}')
 
-        for element in target_weather_elements:
-            name = element['elementName']
+            target_weather_elements = locations[i]['weatherElement']
 
-            mapping = {
-                'Wx': '天氣現象',
-                'PoP': '降雨機率',
-                'MinT':'最低溫',
-                'CI':'體感',
-                'MaxT':'最高溫'
-            }
+            for element in target_weather_elements:
+                name = element['elementName']
 
-            chinese_name = mapping.get(name, name)
-            value = element['time'][0]['parameter']['parameterName']
-            print(f'elementName:{name}({chinese_name}) -> {value}')
-    
-    else:
-        print('Not Found')
+                mapping = {
+                    'Wx': '天氣現象',
+                    'PoP': '降雨機率',
+                    'MinT':'最低溫',
+                    'CI':'體感',
+                    'MaxT':'最高溫'
+                }
+
+                chinese_name = mapping.get(name, name)
+                value = element['time'][0]['parameter']['parameterName']
+                print(f'elementName:{name}({chinese_name}) -> {value}')
+        
+        else:
+            print('Not Found')
 
 
