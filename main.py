32157592daefce692dae1589eb86b93a80f24768
Yuchia <jerryis0812@gmail.com>
2026-01-08 23:25:20 +0800

    API completed

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
-def main():
-    print("Hello from line-weather-bot!")
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+from test_api import get_weather_data, API_KEY
+
+app = FastAPI()
+
+class WeatherQuery(BaseModel):
+    city: str
+
+@app.post("/query_weather")
+def query_weather(item: WeatherQuery):
+    result = get_weather_data(item.city, API_KEY)
+    return {"Reply":result}
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
